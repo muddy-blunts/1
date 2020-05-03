4fd00a55a6cd9074b587d979f8b511b5f8393a77
muddy-blunts <55998224+muddy-blunts@users.noreply.github.com>
2020-05-03 15:34:02 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+"""
+This is a sample config file
+Copy this to config.py and fill in your actual data before starting
+"""
+
+config = {
+    "token": "1132131755:AAG-hM7izUBbfGAB99gdFB75AokO2gbt8N4",
+    "channel": "@pokrovskme",
+    "feeds": ["https://vchasnoua.com/rss", "Sat, 02 May 2020 05:52:48 +0000"]
+}
---
+++ main.py
@@ -0,0 +1,60 @@
+# _*_ coding: utf-8 _*_
+
+from bs4 import BeautifulSoup
+from datetime import datetime
+from config import config
+import feedparser
+import telebot
+import json
+import os
+
+
+def get_timestamp(dt):
+    return datetime.strptime(dt, '%a, %d %b %Y %H:%M:%S %z').timestamp()
+
+
+bot = telebot.TeleBot(config.get('token'))
+
+feeds = list()
+
+if type(config.get('feeds')) == str:
+    feeds.append(feedparser.parse(config.get('feeds')))
+else:
+    for feed in config.get('feeds'):
+        feeds.append(feedparser.parse(feed))
+
+new_posts = []
+
+if not os.path.exists('posts.json'):
+    with open('posts.json', 'w') as f:
+        json.dump([], f)
+        f.close()
+
+with open('posts.json') as f:
+    guids = json.load(f)
+    f.close()
+
+for feed in feeds:
+    for entry in feed.entries:
+        content = entry['description']
+        post = {}
+        soup = BeautifulSoup(content, 'html.parser')
+        if soup.img:
+            post['image'] = soup.img['src']
+        post['text'] = '\n'.join(soup.stripped_strings)
+        post['text'] += '\n\n{}'.format(entry['link'])
+        post['date'] = get_timestamp(entry['published'])
+
+        if entry['guid'] not in guids:
+            # TODO: strip guid with a regex like /d{4,}\/{?}$/
+            new_posts.append(post)
+            guids.append(entry['guid'])
+
+new_posts.sort(key=lambda x: x['date'])
+
+for post in new_posts:
+    bot.send_message(config.get('channel'), post.get('text'))
+
+with open('posts.json', 'w') as f:
+    json.dump(guids, f)
+    f.close()
